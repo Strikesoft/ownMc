e42dacc646ecbd4bd891729d88af185d693fba8b
Johann-S <johann.servoire@gmail.com>
2015-11-25 10:32:31 +0100

    Add some tasks to Grunt (watch, babel, babel-eslint)

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,14 +10,43 @@ module.exports = function (grunt) {
     // Project configuration.
     grunt.initConfig({
         // Metadata.
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        eslint: {
+            options: {
+                configFile: '.eslintrc'
+            },
+            target: 'web/js/*.js'
+        },
+        jscs: {
+            options: {
+                config: '.jscsrc'
+            },
+            core: {
+                src: 'web/js/*.js'
+            }
+        },
+        babel: {
+            dev: {
+                options: {
+                    sourceMap: true,
+                    modules: 'ignore'
+                },
+                files: {}
+            }
+        },
+        watch: {
+            src: {
+                files: '<%= jscs.core.src %>',
+                tasks: ['babel:dev']
+            }
+        }
     });
 
     // These plugins provide necessary tasks.
     require('load-grunt-tasks')(grunt, { scope: 'devDependencies' });
 
     // Default task.
-    grunt.registerTask('default', ['']);
+    grunt.registerTask('default', ['test']);
 
-    grunt.registerTask('test', ['']);
+    grunt.registerTask('test', ['eslint', 'jscs:core']);
 };
