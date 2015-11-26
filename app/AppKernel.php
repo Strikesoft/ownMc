dfbe6f651eb963b28bb77e07fdd9ef6f23a8f125
Johann-S <johann.servoire@gmail.com>
2015-11-26 21:41:25 +0100

    Added FOSUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+
+            // Our Bundles
             new FrontBundle\FrontBundle(),
+            new UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
